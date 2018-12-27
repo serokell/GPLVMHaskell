4f601bb9f901b159d66fb838e241861548a6d76c
DanielRrr <daniel.rogozin@serokell.io>
2018-12-27 18:20:44 +0300

    [AI-15] IVM (wip)

--- src/GPLVM/IVM.hs
+++ src/GPLVM/IVM.hs
@@ -1,6 +1,7 @@
 {-
-based on Informative vector machine implemented as follows:
-https://github.com/lawrennd/ivm/blob/master/matlab/ivmCreate.m
+based on Informative vector machine implemented based on the following:
+http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.440.3620&rep=rep1&type=pdf
+
 -}
 
 module GPLVM.IVM where
@@ -10,3 +11,21 @@ import Universum hiding (Vector)
 import Control.Lens (makeLenses)
 import Data.Array.Repa
 import Numeric.LinearAlgebra.Repa hiding (Matrix, Vector)
+
+import GPLVM.Types
+
+newtype IVMInput a = IVMInput { unInput :: Matrix a }
+
+newtype IVMOutput a = IVMOutput { unOutput :: Matrix a }
+
+----- simple ivm
+
+simpleIVM
+    :: IVMInput a
+    -> Int      --- a new number of rows
+    -> IVMOutput a
+simpleIVM input@(IVMInput matrixInput) =
+
+
+
+----- multitask ivm
